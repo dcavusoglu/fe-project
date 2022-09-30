cf9497e015b649de479b459170938e2c83eb4aee
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-09-30 23:13:41 +0300

    product cardd props

--- src/Pages/Feed.jsx
+++ src/Pages/Feed.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard/ProductCard';
+import Moment from 'moment';
+
+import data from '../api/data.json';
+
 
 const Feed = () => {
+
+console.log(data.posts_by_date['2021-06-17'].map((item)=> item.published_at))
+console.log('datamap',data.posts_by_date)
+const dateFirst = data.posts_by_date['2021-06-17'];
+const dateSecond = data.posts_by_date['2021-07-01'];
+
   return (
-    <div>Feed</div>
+    <div className='flex flex-col mt-4 ml-6'>
+      <div>
+        <span>{Moment('2021-06-17').format('DD MMMM YYYY')}</span>
+        <div className='flex flex-row mt-8 mb-6'>
+          {dateFirst.map((item, index) => <ProductCard item={item} key={index}/>)}
+        </div>
+      </div>
+      <div>
+        <span>{Moment('2021-07-01').format('DD MMMM YYYY')}</span>
+        <div className='flex flex-row mt-8 mb-6'>
+          {dateSecond.map((item, index) => <ProductCard item={item} key={index}/>)}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -9,10 +9,42 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import CheckIcon from '@mui/icons-material/Check';
 import BlockIcon from '@mui/icons-material/Block';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Moment from 'moment';
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
   return (
-    <div>
+    <div className='border-2 border-gray-400 rounded-xl flex flex-row items-center w-64 h-64 mr-8'>
+      <div className='bg-green-400 w-8 h-64 flex flex-row items-center'>
+        <span className='ml-1.5'>
+          {(item.account.channel === 'instagrambusiness') ? <InstagramIcon/>
+          : (item.account.channel === 'facebook') ? <FacebookIcon/>
+          :<TwitterIcon/>}
+        </span>
+      </div>
+      <div className='bg-gray-100 w-56 h-64'>
+        <div className='m-2'>
+          <div>
+            <span className='text-xs text-gray-400'>{Moment(item.published_at).format('DD MMMM YYYY - hh:mm')}</span>
+            <span>
+              <BlockIcon/>
+              <DeleteOutlineIcon/>
+              <MoreHorizIcon className='rounded-full border-2 border-gray-400 text-gray-400'/>
+            </span>
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
 
     </div>
   )
