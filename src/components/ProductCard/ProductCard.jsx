9fc63435251c22d4adf776f998a0870538fb2d4d
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-10-03 13:00:17 +0300

    card bottom

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import ShareIcon from '@mui/icons-material/Share';
@@ -16,6 +15,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import Moment from 'moment';
 import noImage from '../../assets/no-post-image.png';
 import classNames from 'classnames';
+
 const ProductCard = ({item}) => {
   const defaultImage = noImage
   const replaceImage = (error) => {
@@ -35,7 +35,6 @@ const className = classNames({
   return (
     <a href={item.account.link} className='flex flex-row items-center mr-8 productcard-wrapper'>
       <div className={className} >
-        {/* className={`(${item.status} === 0) ? bg-yellow-400 : (${item.status} === 1) ? bg-green-400 : (${item.status} === 2) ? bg-blue-500 : (${item.status} === 3) ? bg-gray-400 : (${item.status} === 4) ? bg-red-400 } */}
         <span>
           {(item.account.channel === 'instagrambusiness') ? <InstagramIcon className='social-icon text-gray-200'/>
           : (item.account.channel === 'facebook') ? <FacebookIcon className='social-icon text-gray-200'/>
@@ -47,7 +46,7 @@ const className = classNames({
           <div className='card-top'>
             <div className='text-m text-gray-400'>{Moment(item.published_at).format('DD MMMM YYYY - hh:mm')}</div>
             <div className='flex items-center'>
-              <BlockIcon className='text-gray-300 card-icon'/>
+              {item.status === 0 ? <BlockIcon className='text-gray-300 card-icon'/> : <CheckIcon className='text-gray-300 card-icon'/> }
               <DeleteOutlineIcon className='text-gray-300 ml-2 card-icon'/>
               <MoreHorizIcon className='rounded-full border-2 border-gray-300 text-gray-300 ml-2 card-icon'/>
             </div>
@@ -59,10 +58,17 @@ const className = classNames({
             <img src={item.entry.image} onError={replaceImage} alt='itemm'/>
           </div>
           <div className='flex items-center'>
-            <div className='text-gray-300 mr-4'>
-              <FavoriteBorderIcon className='card-icon'/>
-              <span>23</span>
-            </div>
+            {(item.account.channel === 'instagrambusiness') || (item.account.channel === 'facebook') ?
+              <div className='text-gray-300 mr-4'>
+                <ThumbUpOffAltIcon className='card-icon'/>
+                <span>23</span>
+              </div>
+                :
+              <div className='text-gray-300 mr-4'>
+                <FavoriteBorderIcon className='card-icon'/>
+                <span>23</span>
+              </div>
+            }
             <div className='text-gray-300 mr-4'>
               <RepeatIcon className='text-gray-300 mr-4 card-icon'/>
               <span>23</span>
