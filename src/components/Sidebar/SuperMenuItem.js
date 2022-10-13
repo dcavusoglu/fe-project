74f7e3c0f307856e564e910d59dd43a9119efe8b
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-10-13 10:34:47 +0300

    super nested menu flex-row

--- src/components/Sidebar/SuperMenuItem.js
+++ src/components/Sidebar/SuperMenuItem.js
@@ -9,16 +9,16 @@ const SuperMenuItem = ({menu, children}) => {
   };
 
   return (
-    <div>
+    <div className='flex flex-row'>
       <ul className={open ? "bg-gray-800 flex justify-content-between" : "mb-0"}>
-      <li onClick={handleClick}>
+      <li onClick={handleClick} className="mb-0">
         <img src={menu.image} alt='logos' className={open ? 'opacity-2 super-logos' : 'super-logos opacity-10'}/>
       </li>
+
+    </ul>
         <Collapse in={open} timeout="auto" unmountOnExit>
           {children}
         </Collapse>
-
-    </ul>
     </div>
   )
 }
